284285bcc7e0e3038741362df2fb57c7430e0aac
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-11-03 11:56:42 -0500

    ajustes

--- src/main/java/com/centroinformacion/controller/LoginController.java
+++ src/main/java/com/centroinformacion/controller/LoginController.java
@@ -31,34 +31,25 @@ public class LoginController {
 	
 	@PostMapping("/login")
 	public String login(Usuario user, HttpSession session, HttpServletRequest request) {
-		
-		
 		Jedis jedis = new Jedis();
 		Gson gson = new Gson();
-		
 		try {
 			//---------------------------------------------
 			//CACHE ==> DINAMICO
-			//---------------------------------------------
-		
+
 			Map<String, String> mapsUsuarios  = jedis.hgetAll("usuarios");
 			Map<String, String> mapsRoles  = jedis.hgetAll("roles");
 			Map<String, String> mapsMenus  = jedis.hgetAll("menus");
-			
 			if (mapsUsuarios.containsKey(user.getLogin())){
 			 	Usuario usuarioRedis =  gson.fromJson(mapsUsuarios.get(user.getLogin()), Usuario.class);
 				if (usuarioRedis.getPassword().equals(user.getPassword())) {
-					
 					Type listRolType = new TypeToken<List<Rol>>() {}.getType();
 					Type listOpcionType = new TypeToken<List<Opcion>>() {}.getType();
-					
 					List<Rol> rolesRedis =  gson.fromJson(mapsRoles.get(user.getLogin()), listRolType);
 					List<Opcion> menusRedis =  gson.fromJson(mapsMenus.get(user.getLogin()), listOpcionType);
-					
 					session.setAttribute("objUsuario", usuarioRedis);
 					session.setAttribute("objMenus", menusRedis);
 					session.setAttribute("objRoles", rolesRedis);
-					
 					return "intranetHome";	
 				}else {
 					request.setAttribute("mensaje", "El usuario no existe");
@@ -75,17 +66,13 @@ public class LoginController {
 					session.setAttribute("objUsuario", usuarioMysql);
 					session.setAttribute("objMenus", menus);
 					session.setAttribute("objRoles", roles);
-					
 					//Se agrega al CACHE el usuario, roles y menus del MYSQL
 					mapsUsuarios.put(usuarioMysql.getLogin(), gson.toJson(usuarioMysql));
 					jedis.hset("usuarios", mapsUsuarios);
-					
 					mapsRoles.put(usuarioMysql.getLogin(), gson.toJson(roles));
 					jedis.hset("roles", mapsRoles);
-					
 					mapsMenus.put(usuarioMysql.getLogin(), gson.toJson(usuarioMysql));
 					jedis.hset("menus", mapsMenus);
-					
 					return "intranetHome";
 				}
 			}
